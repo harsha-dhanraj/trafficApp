ca1b7696ccc4e5d51dad21c2ce11cf908c0392ab
Harshvardhan <harshu.rtk03@gmail.com>
2018-01-26 13:15:54 +0530

    Doctor login done

---
+++ models/doctor.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const doctorSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    specialization:{
+        type:String,
+        required:true
+    },
+    contact_number:{
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const Doctor = module.exports = mongoose.model('doctor', doctorSchema);
+
+module.exports.addDoctor = (doctor, callback) => {
+    console.log("Comming here in addDoctor....")
+    console.log(doctor);
+    Doctor.create(doctor, callback);
+}
+
+module.exports.getDoctorbyEmail = (email, callback) => {
+  console.log("Email is : "+email);
+  var query = {
+    email: email
+  }
+    Doctor.find(query, callback);  
+}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var router = express.Router();
 const Light = require(__dirname + '/models/lights');
+const Doctor = require(__dirname + '/models/doctor');
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -23,6 +24,102 @@ router.use(function (req,res,next) {
 router.get("/",function(req,res){
   res.sendFile(path + "home.html");
 });
+
+router.get("/login",function(req,res){
+  res.sendFile(path + "home.html");
+});
+
+router.get("/register",function(req,res){
+  res.sendFile(path + "register.html");
+});
+
+
+router.post("/register",function(req,res){
+	console.log("Comming hereeeee..... in addDoctor route.");
+	var new_doctor = req.body;
+	var email = new_doctor.email
+	// var doc = Doctor.findOne({email: email}
+	Doctor.findOne({email: email},function(err,doctor){
+
+		console.log("in find one")
+		console.log("doctor is "+doctor)
+		if (doctor !== null){
+			res.json({
+        success:false,
+        msg:"Doctor with this email id is already registered",
+        doctor:doctor
+      });
+		}else{
+			Doctor.addDoctor(new_doctor,(err,doctor)=>{
+		    // console.log(new_doctor);
+		    if (err){
+	        console.error(err);
+	        res.json({
+            success: false,
+            msg: "There is some error"
+	        });
+		    } else {
+		    	res.sendFile(path + "patients.html");
+	        // res.json({
+         //    success:true,
+         //    msg:"Doctor registered successfully....",
+         //    doctor:doctor
+	        // })
+		    }
+			});
+		}
+	})
+	// res.sendFile(path + "patients.html");
+});
+
+// //////////////////////////////////////////////////////////////////
+
+router.post("/addLight",function(req,res){ 
+	console.log("Comming hereeeee......") 
+  var new_light = req.body;
+  var uuid=new_light.uuid
+  var distance = new_light.distance
+  console.log("new light req:"+uuid)
+  // findOne({_id: req.params["_id"]}
+  Light.findOne({uuid: uuid},function(err,new_light){
+  // Light.findOne({},(err,new_light_)=>{
+    if (new_light.length != 0){
+    		if(distance <= threshold){
+    			new_light.status = "ON"
+    		}else{
+    			new_light.status = "OFF"
+    		}
+    		new_light.save();
+        res.json({
+          success:false,
+          msg:"light already registered",
+          light:new_light
+        });
+    }else{
+		  Light.addLight(new_light,(err,light)=>{
+		    console.log(new_light);
+		    if (err){
+	        console.error(err);
+	        res.json({
+            success: false,
+            msg: "There is some error"
+	        });
+		    } else {
+	        res.json({
+            success:true,
+            msg:"Light registered successfully....",
+            light:light
+	        })
+		    }
+			});
+		}
+});
+});
+
+
+/////////////////////////////////////////////////////////////////////
+
+
 
 router.get("/lights",function(req,res){
   // res.sendFile(path + "lights.html");
